97139fa15280426196fb76a60fbb244930649234
Eric Fortis <efortis@gmail.com>
2026-03-25 23:12:13 -0400

    opt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,25 @@
-	plugins {
-		id("java")
-		id("org.jetbrains.intellij.platform") version "2.2.1"
+plugins {
+	id("java")
+	id("org.jetbrains.intellij.platform") version "2.2.1"
+}
+
+
+java {
+	toolchain {
+		targetCompatibility = JavaVersion.VERSION_21
+		sourceCompatibility = JavaVersion.VERSION_21
 	}
+}
 
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
 
-	java {
-		toolchain {
-			targetCompatibility = JavaVersion.VERSION_21
-			sourceCompatibility = JavaVersion.VERSION_21
-		}
+repositories {
+	mavenCentral()
+	intellijPlatform {
+		defaultRepositories()
 	}
-
-	group = providers.gradleProperty("pluginGroup").get()
-	version = providers.gradleProperty("pluginVersion").get()
-
-	repositories {
-		mavenCentral()
-		intellijPlatform {
-			defaultRepositories()
-		}
-	}
+}
 
 dependencies {
 	intellijPlatform {
@@ -30,33 +30,33 @@ dependencies {
 	testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.10.2")
 }
 
-	intellijPlatform {
-		pluginConfiguration {
-			id.set("com.ericfortis.tailwindeye")
-			name.set("Tailwind Eye")
-			vendor {
-				name.set("ericfortis")
-			}
-			changeNotes.set("Initial version of Tailwind Eye.")
-
-			ideaVersion {
-				sinceBuild.set("261")
-			}
+intellijPlatform {
+	pluginConfiguration {
+		id.set("com.ericfortis.tailwindeye")
+		name.set("Tailwind Eye")
+		vendor {
+			name.set("ericfortis")
 		}
+		changeNotes.set("Initial version of Tailwind Eye.")
 
-		pluginVerification {
-			ides {
-				recommended()
-			}
-		}
-
-		signing {
-			certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-			privateKey.set(System.getenv("PRIVATE_KEY"))
-			password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-		}
-
-		publishing {
-			token.set(System.getenv("PUBLISH_TOKEN"))
+		ideaVersion {
+			sinceBuild.set(providers.gradleProperty("pluginSinceBuild").get())
 		}
 	}
+
+	pluginVerification {
+		ides {
+			recommended()
+		}
+	}
+
+	signing {
+		certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+		privateKey.set(System.getenv("PRIVATE_KEY"))
+		password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+	}
+
+	publishing {
+		token.set(System.getenv("PUBLISH_TOKEN"))
+	}
+}
