c00158c24882794a285dc0f5a13af8bdd3c06aad
Eric Fortis <efortis@gmail.com>
2026-03-29 01:02:32 -0400

    tests

--- src/test/java/com/ericfortis/tailwindeye/ExpandClassNameActionTest.java
+++ src/test/java/com/ericfortis/tailwindeye/ExpandClassNameActionTest.java
@@ -3,8 +3,8 @@ package com.ericfortis.tailwindeye;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
 public class ExpandClassNameActionTest extends BasePlatformTestCase {
-	public final String inline = "\"my-2 flex flex-col\"";
-	public final String multiline = "{`\nmy-2\nflex\nflex-col\n`}";
+	public final String inline = "\"flex flex-col\"";
+	public final String multiline = "{`\nflex\nflex-col\n`}";
 
 	public void testToMultiline() {
 		assertEquals(ExpandClassNameAction.toggle(inline), multiline);
