0543e211e31c742b8f1cb13ac7d5814fbf7af00a
Eric Fortis <efortis@gmail.com>
2026-03-29 01:01:45 -0400

    tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
 		plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
 		bundledModules(providers.gradleProperty("platformBundledModules").map { it.split(',') })
 		testFramework(TestFrameworkType.Platform)
-		testFramework(TestFrameworkType.Plugin.JavaScript)
 	}
 }
 
