3d223d4f2ed9cbfb7046b1bd026e499d3fa565ea
Eric Fortis <efortis@gmail.com>
2026-03-29 00:59:55 -0400

    tests

--- src/test/java/com/ericfortis/tailwindeye/ExpandClassNameActionTest.java
+++ src/test/java/com/ericfortis/tailwindeye/ExpandClassNameActionTest.java
@@ -1,35 +1,16 @@
 package com.ericfortis.tailwindeye;
 
-import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
 public class ExpandClassNameActionTest extends BasePlatformTestCase {
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		PluginManagerCore.getLoadedPlugins().forEach(p ->
-			 System.out.println("Loaded: " + p.getPluginId())
-		);
-	}
-
-	public void testJsxFileTypeResolution() {
-		myFixture.configureByText("test.jsx", "<div className=\"x\"></div>");
-		String fileTypeName = myFixture.getFile().getFileType().getName();
-		String languageId = myFixture.getFile().getLanguage().getID();
-		System.out.println("FileType: " + fileTypeName + ", Language: " + languageId);
-		assertFalse("PLAIN_TEXT".equals(myFixture.getFile().getFileType().getName()));
-		assertFalse("TEXT".equals(myFixture.getFile().getLanguage().getID()));
-	}
+	public final String inline = "\"my-2 flex flex-col\"";
+	public final String multiline = "{`\nmy-2\nflex\nflex-col\n`}";
 
 	public void testToMultiline() {
-		myFixture.configureByText("test.jsx", "<div className=\"my-2 <caret>flex flex-col\"></div>");
-		myFixture.testAction(new ExpandClassNameAction());
-		myFixture.checkResult("<div className={`\nmy-2\nflex\nflex-col\n`}></div>");
+		assertEquals(ExpandClassNameAction.toggle(inline), multiline);
 	}
 
 	public void testToInline() {
-		myFixture.configureByText("test.jsx", "<div className={`my-2 <caret>flex flex-col`}></div>");
-		myFixture.testAction(new ExpandClassNameAction());
-		myFixture.checkResult("<div className=\"my-2 flex flex-col\"></div>");
+		assertEquals(ExpandClassNameAction.toggle(multiline), inline);
 	}
 }
