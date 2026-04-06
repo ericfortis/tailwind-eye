ad752ff6c7bae07f19f73b57d83856ca334758f4
Eric Fortis <efortis@gmail.com>
2026-04-06 18:42:35 -0400

    test

--- src/test/java/com/ericfortis/tailwindeye/ActionTransposeTest.java
+++ src/test/java/com/ericfortis/tailwindeye/ActionTransposeTest.java
@@ -3,26 +3,15 @@ package com.ericfortis.tailwindeye;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
 public class ActionTransposeTest extends BasePlatformTestCase {
-	public final String inline = "\"flex flex-col\"";
-	public final String multiline = "{`\nflex\nflex-col\n`}";
-
 	public void testToMultiline() {
-		assertEquals(ActionTranspose.toggle(inline), multiline);
+		myFixture.configureByText("test.jsx", "<div className=\"flex<caret> flex-col\"></div>");
+		myFixture.testAction(new ActionTranspose());
+		myFixture.checkResult("<div className={`\nflex\nflex-col\n`}></div>");
 	}
 
 	public void testToInline() {
-		assertEquals(ActionTranspose.toggle(multiline), inline);
-	}
-
-	public void testToMultilineCaret() {
-		myFixture.configureByText("test.jsx", "<div className=\"my-2 <caret>flex flex-col\"></div>");
+		myFixture.configureByText("test.tsx", "<div className={`flex<caret> flex-col`}></div>");
 		myFixture.testAction(new ActionTranspose());
-		myFixture.checkResult("<div className={`\nmy-2\nflex\nflex-col\n`}></div>");
-	}
-
-	public void testToInlineCaret() {
-		myFixture.configureByText("test.jsx", "<div className={`my-2 <caret>flex flex-col`}></div>");
-		myFixture.testAction(new ActionTranspose());
-		myFixture.checkResult("<div className=\"my-2 flex flex-col\"></div>");
+		myFixture.checkResult("<div className=\"flex flex-col\"></div>");
 	}
 }
