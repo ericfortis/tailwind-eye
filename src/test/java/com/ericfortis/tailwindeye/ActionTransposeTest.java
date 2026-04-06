1f62c739b6c8bfa7e48996459fe17b3ea794edb3
Eric Fortis <efortis@gmail.com>
2026-04-06 16:38:29 -0400

    fix tests jsx

--- src/test/java/com/ericfortis/tailwindeye/ActionTransposeTest.java
+++ src/test/java/com/ericfortis/tailwindeye/ActionTransposeTest.java
@@ -2,6 +2,8 @@ package com.ericfortis.tailwindeye;
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
+import static org.junit.Assert.assertNotEquals;
+
 public class ActionTransposeTest extends BasePlatformTestCase {
 	public final String inline = "\"flex flex-col\"";
 	public final String multiline = "{`\nflex\nflex-col\n`}";
@@ -13,4 +15,26 @@ public class ActionTransposeTest extends BasePlatformTestCase {
 	public void testToInline() {
 		assertEquals(ActionTranspose.toggle(multiline), inline);
 	}
+
+
+	public void testJsxFileTypeResolution() {
+		myFixture.configureByText("test.jsx", "<div className=\"x\"></div>");
+		String fileTypeName = myFixture.getFile().getFileType().getName();
+		String languageId = myFixture.getFile().getLanguage().getID();
+		System.out.println("FileType: " + fileTypeName + ", Language: " + languageId);
+		assertNotEquals("PLAIN_TEXT", myFixture.getFile().getFileType().getName());
+		assertNotEquals("TEXT", myFixture.getFile().getLanguage().getID());
+	}
+
+//	public void testToMultiline() {
+//		myFixture.configureByText("test.jsx", "<div className=\"my-2 <caret>flex flex-col\"></div>");
+//		myFixture.testAction(new ExpandClassNameAction());
+//		myFixture.checkResult("<div className={`\nmy-2\nflex\nflex-col\n`}></div>");
+//	}
+//
+//	public void testToInline() {
+//		myFixture.configureByText("test.jsx", "<div className={`my-2 <caret>flex flex-col`}></div>");
+//		myFixture.testAction(new ExpandClassNameAction());
+//		myFixture.checkResult("<div className=\"my-2 flex flex-col\"></div>");
+//	}
 }
